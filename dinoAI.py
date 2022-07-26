3163530927dbb7b69bbbbaceee18ac0c1e28669b
ezschneider <ezequielschneiderr@gmail.com>
2022-07-26 01:13:42 -0300

    final version

--- dinoAI.py
+++ dinoAI.py
@@ -462,7 +462,7 @@ def generate_initial_state(n_internalNeurons, n_decisionNeurons, params):
     for _ in range(params * (n_decisionNeurons+n_internalNeurons)):
         initial_state.append(random.randint(min_value,max_value))
     np_initial_state = np.asarray(initial_state)
-    np_best_state = [464,569,567,831,56,1,-539,-767,-152,506,49,-139,9,-168,-715,-965,-88,683,136,162]
+    np_best_state = [464,569,567,574,56,15,-539,-767,-186,506,268,-464,9,-168,-715,-965,-88,456,136,162]
     return np_best_state
 
 def selection(value_population,n):
